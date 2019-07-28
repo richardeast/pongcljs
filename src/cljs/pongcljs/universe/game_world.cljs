4a20692bdb6c44cd4545b2bd01f8dbc78b9f9188
richardeast <richard.east@gmail.com>
2019-07-28 10:22:02 +0100

    Minor refactoring to simplify the code

--- src/cljs/pongcljs/universe/game_world.cljs
+++ src/cljs/pongcljs/universe/game_world.cljs
@@ -3,28 +3,26 @@
             [pongcljs.styles :as styles]
             [pongcljs.hex-rgb :as hex]))
 
-
 (defn set-color
   [state path color-position]
-  (let [style (:style state)]
-    (assoc-in state
-              (concat [:universe :game-world :items] path)
-              (styles/style->color style #(nth % color-position)))))
+  (assoc-in state
+            (concat [:universe :game-world :items] path)
+            (styles/n->color state color-position)))
 
 (defn change-colors
   [state]
-  (let [style (:style state)]
-    (-> state
-        (set-color [:ground :colors :fill-color] 2)
-        (set-color [:tennis-court :colors :fill-color] 4)
-        (set-color [:tennis-court :colors :stroke-color] 0)
-        (set-color [:sky :colors :fill-color] 3))))
+  (-> state
+      (set-color [:ground :colors :fill-color] 2)
+      (set-color [:tennis-court :colors :fill-color] 4)
+      (set-color [:tennis-court :colors :stroke-color] 0)
+      (set-color [:sky :colors :fill-color] 3)))
 
 (defn horizon-height [state]
   (get-in state [:universe :game-world :horizon]))
 
 (defn draw-sky [state]
   ;; TODO make the sky more interesting
+  ;; https://p5js.org/examples/color-linear-gradient.html
   (let [sky-color (get-in state [:universe :game-world :items
                                     :sky :colors :fill-color])]
     (hex/background sky-color)))
@@ -55,18 +53,11 @@
 (defn draw-court-quadrant [state]
   (let [[w h] (get-in state [:screen :size])
         court-top (horizon-height state)
-        court-quadant {:top-left [(/ w 4.25) court-top]
-                       :top-right [(- w (/ w 4.25)) court-top]
-                       :bottom-right [w h]
-                       :bottom-left [0 h]}
-        x1 (first (:top-left court-quadant))
-        y1 (second (:top-left court-quadant))
-        x2 (first (:top-right court-quadant))
-        y2 (second (:top-right court-quadant))
-        x3 (first (:bottom-right court-quadant))
-        y3 (second (:bottom-right court-quadant))
-        x4 (first (:bottom-left court-quadant))
-        y4 (second (:bottom-left court-quadant))]
+        ;; court-quadrant {:top-left :top-right :bottom-right :bottom-left }
+        [x1 y1 x2 y2 x3 y3 x4 y4] [(/ w 4.25) court-top
+                                   (- w (/ w 4.25)) court-top
+                                   w h
+                                   0 h]]
     (q/quad x1 y1
             x2 y2
             x3 y3
@@ -86,8 +77,8 @@
     (q/stroke-weight stroke-weight)
     (draw-court-quadrant state)
     (q/line 172 (/ h 3.5) 677 (/ h 3.5))
-    (q/ellipse centre-x centre-y 200 75)
     (q/line 55 (/ h 1.3) 794 (/ h 1.3))
+    (q/ellipse centre-x centre-y 200 75)
     ;;TODO These have side effects later on. Delete and fix the issue they cause.
     (q/stroke 0)
     (q/stroke-weight 1)
